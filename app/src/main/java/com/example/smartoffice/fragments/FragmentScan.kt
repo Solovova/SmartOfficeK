c85be2ee6bc006c4c113ec468b03a40343f80fbe
Solonichniy Volodimir <vsolo0@gmail.com>
2019-05-10 23:25:44 +0300

    com.notbytes add

--- app/src/main/java/com/example/smartoffice/fragments/FragmentScan.kt
+++ app/src/main/java/com/example/smartoffice/fragments/FragmentScan.kt
@@ -9,12 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.google.android.gms.vision.barcode.Barcode
-import info.androidhive.barcode.BarcodeReader
 import com.example.smartoffice.R
+import com.notbytes.barcode_reader.BarcodeReaderFragment
 
 
-class FragmentScan : Fragment(), BarcodeReader.BarcodeReaderListener {
-    private var barcodeReader: BarcodeReader? = null
+class FragmentScan : Fragment(), BarcodeReaderFragment.BarcodeReaderListener {
+    private var barcodeReader: BarcodeReaderFragment? = null
     private var useFlash: Boolean = false
 
 
@@ -26,7 +26,7 @@ class FragmentScan : Fragment(), BarcodeReader.BarcodeReaderListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_fragment_scan, container, false)
-        barcodeReader = childFragmentManager.findFragmentById(R.id.barcode_fragment) as BarcodeReader
+        barcodeReader = childFragmentManager.findFragmentById(R.id.barcode_fragment) as BarcodeReaderFragment
         barcodeReader?.setListener(this)
         return view
     }
